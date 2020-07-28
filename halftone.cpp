27e3af3342588c7878d4bf4b489cbd9a502b3743
twittman <2048rsa@runbox.com>
2020-07-28 19:17:24 +0100

    Update halftone.cpp
    Added randomisation between four halftone patterns

--- halftone.cpp
+++ halftone.cpp
@@ -1,4 +1,5 @@
 #include <Magick++.h>
+#include <sstream>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -10,14 +11,14 @@ using namespace std;
 using namespace Magick;
 namespace fs = filesystem;
 
-// Rotation values for Dithering Operation
-const double cyanArgsFirst[2] = { 15,1 };
-const double cyanArgsSecond[2] = { -15,1 };
-const double magentaArgsFirst[2] = { 75,1 };
-const double magentaArgsSecond[2] = { -75,1 };
-const double blackArgsFirst[2] = { 45,1 };
-const double blackArgsSecond[2] = { -45,1 };
-
+int whichAnglesYo()
+{
+	const int anglesList[4] = {  1,
+								 2,
+								 3,
+								 4 };
+	return anglesList[rand() % 4];
+}
 
 string samplesYo()
 {
@@ -30,6 +31,21 @@ string samplesYo()
 	return samplesList[rand() % 6];
 }
 
+
+
+double magentaArgsFirst[2];
+double magentaArgsSecond[2];
+double blackArgsFirst[2];
+double blackArgsSecond[2];
+double cyanArgsFirst[2];
+double cyanArgsSecond[2];
+double yellowArgsFirst[2];
+double yellowArgsSecond[2];
+
+
+void case_One(Image& inputCyan, Image& inputMagenta, Image& inputBlack, Image& inputYellow, \
+	double c, double m, double y, double b,\
+	string& ditherTypeThingy, string& croppingThing);
 
 // Random Resize Values for small dither
 const int reSizeMin = 140;
@@ -47,7 +63,6 @@ const double finalGrainMin = 0.05;
 const double finalGrainMax = 0.40;
 
 
-
 // Main Function
 int main(int argc, char** argv)
 {
@@ -55,7 +70,13 @@ int main(int argc, char** argv)
 	const fs::path inputDir{ argc >= 2 ? argv[1] : fs::current_path() };
 	fs::path outputDir(argv[2]);
 
+	// Min and Max Gamma adjustment
+	//const double gammaMin = 0.9;
+	//const double gammaMax = 1.2;
+
+
 	// Minimum and Maximum JPEG Compression values
+	// you can change these for sure.
 	const int qMin = 50;
 	const int qMax = 90;
 
@@ -102,6 +123,11 @@ int main(int argc, char** argv)
 			uniform_int_distribution<int> seq6(quantMin, quantMax);
 			int randomQuant = seq6(randomNumberGen01);
 
+			// random gamma
+			//uniform_real_distribution<double> seq_01(gammaMin, gammaMax);
+			//double gammaRandom = seq_01(randomNumberGen01);
+
+
 			// Random JPEG quality
 			uniform_int_distribution<int> seq2(qMin, qMax);
 			int qJpeg = seq2(randomNumberGen01);
@@ -131,16 +157,8 @@ int main(int argc, char** argv)
 			uniform_real_distribution<double> sequence03(finalGrainMin, finalGrainMax);
 			double finalGrain = sequence03(randomNumberGen01);
 
-			string ditherType;
-			if (randomSolid < 100) {
-				cout << "Solid Black selected";
-			}
-			if (randomLarge <= poLarge) {
-				cout << endl << inFileNoEXT << " Is being processed with these values: " << endl << "Dither Type: " << "h16x16o" << endl << "UpScale: " << randomScalePercentL << endl;
-			}
-			else {
-				cout << endl << inFileNoEXT << " Is being processed with these values: " << endl << "Dither Type: " << "h8x8o" << endl << "UpScale: " << randomScalePercent << endl;
-			}
+			//string ditherType;
+			
 
 
 			try {
@@ -169,7 +187,7 @@ int main(int argc, char** argv)
 				// Generate Noise Map for Displace PER CHANNEL
 				/////////////////////////////////////////////////
 				string noiseSize = to_string(width) + "x" + to_string(height);
-				cout << "original Resolution: " << noiseSize << endl << endl;
+				cout << "original Resolution: " << noiseSize << endl;
 
 				Image noiseForDisplaceCyan(Geometry(noiseSize), "gray"), noiseForDisplaceMagenta(Geometry(noiseSize), "gray"), noiseForDisplaceYellow(Geometry(noiseSize), "gray"), noiseForDisplaceBlack(Geometry(noiseSize), "gray");
 
@@ -203,6 +221,8 @@ int main(int argc, char** argv)
 
 				// Resize Input Image and adjust gamma in Memory before Splitting
 				if (randomLarge <= poLarge) {
+					//inputReScale.gamma(gammaRandom);
+					//inputReScale.modulate(97, 99, 100);
 					inputReScale.colorSpace(CMYKColorspace);
 					inputReScale.filterType(HanningFilter);
 					inputReScale.resize(randomScalePercentL);
@@ -213,6 +233,8 @@ int main(int argc, char** argv)
 					noiseForDisplaceBlack.resize(randomScalePercentL);
 				}
 				else {
+					//inputReScale.gamma(gammaRandom);
+					//inputReScale.modulate(97, 99, 100);
 					inputReScale.colorSpace(CMYKColorspace);
 					inputReScale.filterType(HanningFilter);
 					inputReScale.resize(randomScalePercent);
@@ -240,76 +262,77 @@ int main(int argc, char** argv)
 				inputBlack = inputReScale;
 				inputBlack.channel(BlackChannel);
 
-				// Output Cyan Dithered Image
+				string ditherSize;
+				string cropThis;
+				string forPrint;
+
 				if (randomLarge <= poLarge) {
-					string artARG = "2x2";
-					inputCyan.addNoise(GaussianNoise, 0.21);
-					inputCyan.modulate(100, 0, 100);
-					inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsFirst, Magick::MagickTrue);
-					inputCyan.orderedDither("h16x16o");
-					inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsSecond, Magick::MagickTrue);
-					inputCyan.crop(cropChannelsNewL);
-					inputCyan.artifact("compose:args", artARG);
-					inputCyan.composite(noiseForDisplaceCyan, 0, 0, DisplaceCompositeOp);
+					ditherSize = "h16x16o";
+					cropThis = cropChannelsNewL;
+					forPrint = "h16x16o dither selected";
 				}
 				else {
-					string artARG = "2x2";
-					inputCyan.addNoise(GaussianNoise, 0.11);
-					inputCyan.modulate(100, 0, 100);
-					inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsFirst, Magick::MagickTrue);
-					inputCyan.orderedDither("h8x8o");
-					inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsSecond, Magick::MagickTrue);
-					inputCyan.crop(cropChannelsNew);
-					inputCyan.artifact("compose:args", artARG);
-					inputCyan.composite(noiseForDisplaceCyan, 0, 0, DisplaceCompositeOp);
+					ditherSize = "h8x8o";
+					cropThis = cropChannelsNew;
+					forPrint = "h8x8o dither selected";
 				}
-				//inputCyan.write("_cyan.png");
+
+				if (randomSolid < 100) {
+					cout << "\nSolid Black has been selected for the Image below";
+				}
+				else {
+					cout << endl << inFileNoEXT << " Is being processed with these values: " << endl << "UpScale: " << randomScalePercent << endl;
+				}
+
+				switch (whichAnglesYo())
+				{
+				case 1:
+					// One	
+					case_One(inputCyan, inputMagenta, inputYellow, inputBlack, 15, 75, 0, 45, ditherSize, cropThis);
+					cout << "\nDither angles #1" << endl;
+					break;
+				case 2:
+					// Two
+					case_One(inputCyan, inputMagenta, inputYellow, inputBlack, 105, 75, 90, 15, ditherSize, cropThis);
+					cout << "\nDither angles #2" << endl;
+					break;
+				case 3:
+					// Three
+					case_One(inputCyan, inputMagenta, inputYellow, inputBlack, 15, 45, 0, 75, ditherSize, cropThis);
+					cout << "\nDither angles #3" << endl;
+					break;
+				case 4:
+					// Four
+					case_One(inputCyan, inputMagenta, inputYellow, inputBlack, 165, 45, 90, 105, ditherSize, cropThis);
+					cout << "\nDither angles #4" << endl;
+					break;
+				}
+
+				string artARG = "2x2";
+
+				// Output Cyan Dithered Image
+				inputCyan.addNoise(GaussianNoise, 0.15);
+				inputCyan.modulate(100, 0, 100);
+				case_One;
+				inputCyan.artifact("compose:args", artARG);
+				inputCyan.composite(noiseForDisplaceCyan, 0, 0, DisplaceCompositeOp);
+				inputCyan.write("_cyan.png");
 
 				// Output Magenta Dithered Image
-				if (randomLarge <= poLarge) {
-					string artARG = "2x2";
-					inputMagenta.addNoise(GaussianNoise, 0.13);
-					inputMagenta.modulate(100, 0, 100);
-					inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsFirst, Magick::MagickTrue);
-					inputMagenta.orderedDither("h16x16o");
-					inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsSecond, Magick::MagickTrue);
-					inputMagenta.crop(cropChannelsNewL);
-					inputMagenta.artifact("compose:args", artARG);
-					inputMagenta.composite(noiseForDisplaceMagenta, 0, 0, DisplaceCompositeOp);
-				}
-				else {
-					string artARG = "2x2";
-					inputMagenta.addNoise(GaussianNoise, 0.13);
-					inputMagenta.modulate(100, 0, 100);
-					inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsFirst, Magick::MagickTrue);
-					inputMagenta.orderedDither("h8x8o");
-					inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsSecond, Magick::MagickTrue);
-					inputMagenta.crop(cropChannelsNew);
-					inputMagenta.artifact("compose:args", artARG);
-					inputMagenta.composite(noiseForDisplaceMagenta, 0, 0, DisplaceCompositeOp);
-				}
-				//inputMagenta.write("_Magenta.png");
+				inputMagenta.addNoise(GaussianNoise, 0.13);
+				inputMagenta.modulate(100, 0, 100);
+				case_One;
+				inputMagenta.artifact("compose:args", artARG);
+				inputMagenta.composite(noiseForDisplaceMagenta, 0, 0, DisplaceCompositeOp);
+				inputMagenta.write("_Magenta.png");
 
 				// Output Yellow Dithered Image
-				if (randomLarge <= poLarge) {
-					string artARG = "2x2";
-					inputYellow.addNoise(GaussianNoise, 0.12);
-					inputYellow.modulate(100, 0, 100);
-					inputYellow.orderedDither("h16x16o");
-					inputYellow.crop(cropChannelsNewL);
-					inputYellow.artifact("compose:args", artARG);
-					inputYellow.composite(noiseForDisplaceYellow, 0, 0, DisplaceCompositeOp);
-				}
-				else {
-					string artARG = "2x2";
-					inputYellow.addNoise(GaussianNoise, 0.12);
-					inputYellow.modulate(100, 0, 100);
-					inputYellow.orderedDither("h8x8o");
-					inputYellow.crop(cropChannelsNew);
-					inputYellow.artifact("compose:args", artARG);
-					inputYellow.composite(noiseForDisplaceYellow, 0, 0, DisplaceCompositeOp);
-				}
-				//inputYellow.write("_Yellow.png");
+				inputYellow.addNoise(GaussianNoise, 0.12);
+				inputYellow.modulate(100, 0, 100);
+				case_One;
+				inputYellow.artifact("compose:args", artARG);
+				inputYellow.composite(noiseForDisplaceYellow, 0, 0, DisplaceCompositeOp);
+				inputYellow.write("_Yellow.png");
 
 				// Output Black Dithered/solid Image
 				if (randomSolid < 100) {
@@ -319,7 +342,7 @@ int main(int argc, char** argv)
 						inputBlack.modulate(100, 0, 100);
 						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsFirst, Magick::MagickTrue);
 						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsSecond, Magick::MagickTrue);
-						inputBlack.crop(cropChannelsNewL);
+						inputBlack.crop(cropThis);
 						inputBlack.artifact("compose:args", artARG);
 						inputBlack.composite(noiseForDisplaceBlack, 0, 0, DisplaceCompositeOp);
 					}
@@ -329,37 +352,21 @@ int main(int argc, char** argv)
 						inputBlack.modulate(100, 0, 100);
 						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsFirst, Magick::MagickTrue);
 						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsSecond, Magick::MagickTrue);
-						inputBlack.crop(cropChannelsNew);
+						inputBlack.crop(cropThis);
 						inputBlack.artifact("compose:args", artARG);
 						inputBlack.composite(noiseForDisplaceBlack, 0, 0, DisplaceCompositeOp);
 					}
 
 				}
 				else {
-					if (randomLarge <= poLarge) {
-						string artARG = "2x2";
-						inputBlack.addNoise(GaussianNoise, 0.09);
-						inputBlack.modulate(100, 0, 100);
-						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsFirst, Magick::MagickTrue);
-						inputBlack.orderedDither("h16x16o");
-						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsSecond, Magick::MagickTrue);
-						inputBlack.crop(cropChannelsNewL);
-						inputBlack.artifact("compose:args", artARG);
-						inputBlack.composite(noiseForDisplaceBlack, 0, 0, DisplaceCompositeOp);
-					}
-					else {
-						string artARG = "2x2";
-						inputBlack.addNoise(GaussianNoise, 0.09);
-						inputBlack.modulate(100, 0, 100);
-						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsFirst, Magick::MagickTrue);
-						inputBlack.orderedDither("h8x8o");
-						inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsSecond, Magick::MagickTrue);
-						inputBlack.crop(cropChannelsNew);
-						inputBlack.artifact("compose:args", artARG);
-						inputBlack.composite(noiseForDisplaceBlack, 0, 0, DisplaceCompositeOp);
-					}
+					string artARG = "2x2";
+					inputBlack.addNoise(GaussianNoise, 0.09);
+					inputBlack.modulate(100, 0, 100);
+					case_One;
+					inputBlack.artifact("compose:args", artARG);
+					inputBlack.composite(noiseForDisplaceBlack, 0, 0, DisplaceCompositeOp);
 				}
-				//inputBlack.write("_Black.png");
+				inputBlack.write("_Black.png");
 
 				// Output Combined Channels to single Colour Image
 				std::vector<Image> imageList;
@@ -373,8 +380,8 @@ int main(int argc, char** argv)
 				Magick::combineImages(&rComposite, imageList.begin(), imageList.end(), AllChannels, CMYKColorspace);
 
 				if (randomSolid <= poJPG) {
-					cout << inFileEXT << endl << "Quantized: " << randomQuant << endl << " and compressed " << endl << endl;
-					cout << inFileEXT << endl << "has been processed and compressed " << endl << endl;
+					cout << inFileEXT << endl << "Quantized: " << randomQuant << endl << " and compressed " << endl;
+					cout << inFileEXT << endl << "has been processed and compressed " << endl;
 					rComposite.gaussianBlur(0, randomFinalBlur);
 					rComposite.filterType(CubicFilter);
 					rComposite.resize(noiseSize);
@@ -410,3 +417,46 @@ int main(int argc, char** argv)
 		}
 	}
 }
+
+
+
+void case_One(Image& inputCyan, Image& inputMagenta, Image& inputBlack, Image& inputYellow, \
+	double c, double m, double y, double b, string& ditherTypeThingy, \
+	string& croppingThing)
+{
+	double cyanArgsFirst[2] = { c,1 };
+	double cyanArgsSecond[2] = { -c,1 };
+	double magentaArgsFirst[2] = { m,1 };
+	double magentaArgsSecond[2] = { -m,1 };
+	double yellowArgsFirst[2] = { y,1 };
+	double yellowArgsSecond[2] = { -y,1 };
+	double blackArgsFirst[2] = { b,1 };
+	double blackArgsSecond[2] = { -b,1 };
+
+	cout << "Dither type: " << ditherTypeThingy << endl;
+
+	// Cyan
+	inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsFirst, Magick::MagickTrue);
+	inputCyan.orderedDither(ditherTypeThingy);
+	inputCyan.distort(ScaleRotateTranslateDistortion, 1, cyanArgsSecond, Magick::MagickTrue);
+	inputCyan.crop(croppingThing);
+
+	// Magenta
+	inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsFirst, Magick::MagickTrue);
+	inputMagenta.orderedDither(ditherTypeThingy);
+	inputMagenta.distort(ScaleRotateTranslateDistortion, 1, magentaArgsSecond, Magick::MagickTrue);
+	inputMagenta.crop(croppingThing);
+
+	// Yellow
+	inputYellow.distort(ScaleRotateTranslateDistortion, 1, yellowArgsFirst, Magick::MagickTrue);
+	inputYellow.orderedDither(ditherTypeThingy);
+	inputYellow.distort(ScaleRotateTranslateDistortion, 1, yellowArgsSecond, Magick::MagickTrue);
+	inputYellow.crop(croppingThing);
+
+	// Black
+	inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsFirst, Magick::MagickTrue);
+	inputBlack.orderedDither(ditherTypeThingy);
+	inputBlack.distort(ScaleRotateTranslateDistortion, 1, blackArgsSecond, Magick::MagickTrue);
+	inputBlack.crop(croppingThing);
+}
+
